e2d34170b91654684c9720049c32357621103bf4
Hanna Rogalska <hanrogalska@gmail.com>
2023-03-09 14:02:10 +0100

    added task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,27 @@
-const buttonStart = document.querySelector('[data-start]');
-console.log(buttonStart);
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let intervalId;
+btnStart.addEventListener('click', onBtnStartClick);
+
+function onBtnStartClick() {
+
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+    
+}
+
+btnStop.addEventListener('click', onBtnStopClick);
+
+function onBtnStopClick() {
+
+    clearInterval(intervalId);
+    
+}
+
